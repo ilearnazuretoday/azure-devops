f8e06260e72f6fa5f9229bf553d09784eb90ae3d
decoder <decoder@lilve.de>
2021-07-26 20:18:44 +0200

    sw

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,5 +3,5 @@ output "storage_account_name" {
 }
 
 output "static_website_address" {
-  value = azure_storage_account.primary_web_endpoint
+  value = azure_storage_account.static_website.primary_web_endpoint
 }
